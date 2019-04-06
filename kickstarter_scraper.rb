aa73bf7312c5fc771103717de36f61a2e7285e31
Malcolm Leake <echelon1089@gmail.com>
2019-04-06 23:01:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,9 +11,8 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  
+  projects = {}
   
   binding.pry
 end
 
-projects = {}
